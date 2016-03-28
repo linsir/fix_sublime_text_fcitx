6b627dc8e16f8bac68f690d0531912f07fe65eeb
vi5i0n <vi5i0n@hotmail.com>
2016-03-28 15:07:11 +0800

    fix

--- sublime_text3_with_fcitx/install_sublime_text_with_fix_im.sh
+++ sublime_text3_with_fcitx/install_sublime_text_with_fix_im.sh
@@ -9,11 +9,11 @@
 #     echo "You are just an ordinary user (but mom loves you as the same)."
 # fi
 
-sudo mv sublime_text/ /opt/sublime_text/
+sudo cp sublime_text/ /opt/sublime_text/
 sudo ln -s /opt/sublime_text /usr/local/sublime_text
 sudo ln -s /usr/local/sublime_text/sublime_text /usr/local/bin/sublime_text
-sudo mv libsublime_imfix.so /usr/lib/
-sudo mv sublime_text.desktop /usr/share/applications/
-mv sublime-text3 ~/.config/
-rm -rf ../sublime_text3_with_fcitx/
+sudo cp libsublime-imfix.so /usr/lib/
+sudo cp sublime_text.desktop /usr/share/applications/
+# mv sublime-text3 ~/.config/
+# rm -rf ../sublime_text3_with_fcitx/
 exit 0
