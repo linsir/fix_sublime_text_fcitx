87721c3977170dbec5d77c49e0dbfc2d858d6014
vi5i0n <vi5i0n@hotmail.com>
2016-03-29 10:48:06 +0800

    fix

--- sublime_text3_with_fcitx/sublime-imfix.c
+++ sublime_text3_with_fcitx/sublime-imfix.c
@@ -5,7 +5,7 @@
    By whitequark@whitequark.org
 
    How to compile:
-   gcc -shared -o libsublime-imfix.so sublime_imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
+   gcc -shared -o libsublime-imfix.so sublime-imfix.c  `pkg-config --libs --cflags gtk+-2.0` -fPIC
    How to use:
    LD_PRELOAD=./libsublime-imfix.so sublime_text
 
