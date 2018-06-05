2d62d5f255f2b5b02fa040eeecccb29525a579c8
Linsir <linsir@live.com>
2018-06-05 21:25:56 +0800

    新增3.1 3176 可用注册码

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 ## fix_sublime_text_with_fcitx
-sublime_text 中文输入
 
-- OS: Kali Linux 2.0
+解决 `sublime_text` 中文输入
+
+- OS: Kali Linux ro
 
 - fcitx version: 4.2.8.5
 
-### usage:
+### Usage:
 
     chmod +x install_sublime_text_with_fix_im.sh && bash install_sublime_text_with_fix_im.sh
+
+### Install
+
+    curl -L https://raw.githubusercontent.com/linsir/fix_sublime_text_fcitx/master/install_sublime_text_with_fix_im.sh | bash
--- install_sublime_text_with_fix_im.sh
+++ install_sublime_text_with_fix_im.sh
@@ -9,11 +9,8 @@
 #     echo "You are just an ordinary user (but mom loves you as the same)."
 # fi
 
-sudo cp -rf sublime_text/ /opt/
-sudo ln -s /opt/sublime_text/sublime /usr/bin/sublime
-# sudo ln -s /usr/local/sublime_text/sublime_text /usr/local/bin/sublime_text
-sudo cp libsublime-imfix.so /usr/lib/
-sudo cp sublime_text.desktop /usr/share/applications/
-# mv sublime-text3 ~/.config/
-# rm -rf ../sublime_text3_with_fcitx/
+
+sudo wget https://raw.githubusercontent.com/linsir/fix_sublime_text_fcitx/master/libsublime-imfix.so /usr/lib/
+sudo wget https://raw.githubusercontent.com/linsir/fix_sublime_text_fcitx/master/sublime_text.desktop /usr/share/applications/
+
 exit 0
